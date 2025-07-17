3df80d52aee544979fd559f0586d3a82ef6d5c68
Yanuar Eka <pramudyanuar@gmail.com>
2025-07-17 23:17:46 +0700

    feat: setup ci/cd

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import './App.css'
-import { ThemeProvider } from './components/theme/theme-provider'
-import { LoginForm } from './components/ui/login-form'
+import { ThemeProvider } from './theme/theme-provider'
 
 function App() {
 
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <div className="flex items-center justify-center min-h-screen bg-background">
-        <LoginForm className="w-full max-w-md" />
+        <h1 className="text-2xl font-bold text-primary-foreground">
+          Welcome to the Vite UI Theme Example
+        </h1> 
       </div>
     </ThemeProvider>
   )
